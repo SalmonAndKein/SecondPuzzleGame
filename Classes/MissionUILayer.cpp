ccf9821c3133ee911d46fd3d5f1bc956a7a75c1d
wintersalmon <salmonjoon@gmail.com>
2015-03-13 22:07:34 +0900

    commit files from Xcode

---
+++ Classes/MissionUILayer.cpp
@@ -0,0 +1,9 @@
+//
+//  MissionUILayer.cpp
+//  SecondPuzzleGame
+//
+//  Created by Salmon on 2015. 3. 11..
+//
+//
+
+#include "MissionUILayer.h"
---
+++ Classes/MissionUILayer.h
@@ -0,0 +1,17 @@
+//
+//  MissionUILayer.h
+//  SecondPuzzleGame
+//
+//  Created by Salmon on 2015. 3. 11..
+//
+//
+
+#ifndef __SecondPuzzleGame__MissionUILayer__
+#define __SecondPuzzleGame__MissionUILayer__
+
+#include "MissionLayer.h"
+
+class MissionUILayer : public MissionLayer {
+    
+};
+#endif /* defined(__SecondPuzzleGame__MissionUILayer__) */
